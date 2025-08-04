f2b50c79b5cd872fff6c7cadd0fd9270b28856c3
RunCodeRun-Pablo <pablomarlop98@gmail.com>
2025-08-04 22:27:17 +0200

    Add ex_list.py, new exercise ex_list.py

--- ex_intstr.py
+++ ex_intstr.py
@@ -1,4 +1,4 @@
- # Function to findg the length of the longest longest substring without repeating characters within a given substring
+# Function to find the length of the longest longest substring without repeating characters within a given substring
 # ie within pwwke it would be 3 (wke)
 
 def longest_substr(s: str) -> int:
@@ -12,6 +12,19 @@ def longest_substr(s: str) -> int:
         result = max(result, ind - first + 1)
 
     return result   # This function maintains a stable window of unrepeated chars all over the string
+
+
+# Function to find the middle character of a string
+# If the string is even it should return two characters
+# If the string is odd it should return one character
+
+def middle (text: str) -> str:
+    l = len(text)
+    if l % 2 == 0:
+        return text[(l//2)-1:(l//2)+1]
+    else:
+        return text [(l//2)]
+
 
 
     
